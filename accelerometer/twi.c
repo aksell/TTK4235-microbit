d1b6ab568146e8eb0c8da504476af5d8eadf514a
Aksel Lenes <aksel.lenes@gmail.com>
2018-04-05 13:58:17 +0200

    WIP twi_init()

--- accelerometer/twi.c
+++ accelerometer/twi.c
@@ -1,6 +1,10 @@
-#includ "twi.h"
+#include "twi.h"
 
+//akiver TWI mod m- riktige signalinjene og 100kbps, signallinjer pmå akriveres av GPIO
 void twi_init(){
+	GPIO->CNF[0] = (1<<3)|(1<<2); 	// set SCL to input, activate pullup
+	GPIO->CNF[30] = (1 << 16); 	// set SDA to input
+
 
 
 }
