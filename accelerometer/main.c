4b2551b8d942f43f749975dc93c04b00b935e403
Aksel Lenes <aksel.lenes@gmail.com>
2018-04-06 11:44:23 +0200

    Add main.c
    I am trying to test twi_multi_read by reading WHO_AM_I from
    the accelerometer and sending it over uart with the lightweight
    print function utility_print.

---
+++ accelerometer/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdint.h> //needed?
+
+#include "twi.h"
+#include "utility.h"
+#include "uart.h"
+
+int main(){
+	//accel_init(); //accel init requires that you implement twi_multi_read first, not needed
+	//to read who_am_i
+
+	uint8_t WHO_AM_I_ACCEL_VALUE = 0x5A; //acelerometer device address
+	uint8_t WHO_AM_I_ACCEL_ADR = 0x0D; //address where WHO_AM_I is stored
+
+	uint8_t accel_addres = 0x1D; //accelerometer slave address
+
+	uint8_t register_to_read = 1;
+	uint8_t * data_buffer;
+	data_buffer = (uint8_t *)malloc(8 * sizeof(uint8_t));
+
+	twi_multi_read(accel_addres, WHO_AM_I_ACCEL_ADR, register_to_read, data_buffer);
+
+	utility_print(&uart_send, "Num: %d\n\r", data_buffer[0]);//write address over uart. takes only whole number does no error checking
+	//utility print can be used with many different mediums uart or a physical printre
+	//use putc to read uart
+	
+	free(data_buffer);
+
+
+	return 0;
+}
+
+// /r is carriage return, 
