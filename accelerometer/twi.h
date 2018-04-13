7a11d6c7ecdb976143acd802d753fb9e911fb77c
Aksel Lenes <aksel.lenes@gmail.com>
2018-04-13 07:56:50 +0200

    Remove tabs

--- accelerometer/twi.h
+++ accelerometer/twi.h
@@ -8,18 +8,18 @@
 #define TWI0 ((NRF_TWI_REG*)0x40003000)
 
 typedef struct {
-	/*Taskts*/
+    /*Taskts*/
     volatile uint32_t STARTRX;
     volatile uint32_t RESERVED0[1];
     volatile uint32_t STARTTX;
     volatile uint32_t RESERVED1[2];
     volatile uint32_t STOP;
     volatile uint32_t RESERVED2[1];
-	volatile uint32_t SUSPEND;
+    volatile uint32_t SUSPEND;
     volatile uint32_t RESUME;
     volatile uint32_t RESERVED3[56];
 
-	/*Events*/
+    /*Events*/
     volatile uint32_t STOPPED;
     volatile uint32_t RXDREADY;
     volatile uint32_t RESERVED4[4];
@@ -30,9 +30,9 @@ typedef struct {
     volatile uint32_t BB;
     volatile uint32_t RESERVED7[49];
 
-	/*Registers*/
+    /*Registers*/
     volatile uint32_t SHORTS;
-    volatile uint32_t RESERVED8[63]; 
+    volatile uint32_t RESERVED8[63];
     volatile uint32_t INTEN;
     volatile uint32_t INTENSET;
     volatile uint32_t INTENCLR;
@@ -55,6 +55,6 @@ typedef struct {
 void twi_init();
 
 void twi_multi_read(uint8_t slave_address, uint8_t start_register,
-int registers_to_read, uint8_t * data_buffer);
+                    int registers_to_read, uint8_t * data_buffer);
 
 #endif
